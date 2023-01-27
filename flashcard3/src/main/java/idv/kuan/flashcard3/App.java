e433a4c501f7bcdc179f57bec3891ae1c40c3bad
Kuanwei <Kuanwei@DESKTOP-Q3LU8R3>
2023-01-28 01:22:43 +0800

    init

---
+++ flashcard3/src/main/java/idv/kuan/flashcard3/App.java
@@ -0,0 +1,13 @@
+package idv.kuan.flashcard3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
