c4810c69439fc28539dd6d46f52af047eef905c5
Kuanwei <Kuanwei@DESKTOP-Q3LU8R3>
2023-01-28 23:38:31 +0800

    db connnecte test

--- flashcard3/src/main/java/idv/kuan/flashcard3/App.java
+++ flashcard3/src/main/java/idv/kuan/flashcard3/App.java
@@ -1,13 +1,33 @@
 package idv.kuan.flashcard3;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		String url = "jdbc:sqlite:C:/java/db/sqlite/flashcard3/fc3.db";
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection connection = DriverManager.getConnection(url);
+			String sql = "insert into word (term,translation)values(?,?)";
+			PreparedStatement prepareStatement = connection
+					.prepareStatement(sql);
+			prepareStatement.setString(1, "apple");
+			prepareStatement.setString(2, "蘋果");
+
+			prepareStatement.executeUpdate();
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Hello World!");
+
+	}
 }
